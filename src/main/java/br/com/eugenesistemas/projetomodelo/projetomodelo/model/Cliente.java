6d66dde869a93d86ecd146fcae23084aa0c9adda
Patrick Eugene de Farias <30466561+patrickfarias@users.noreply.github.com>
2022-06-13 22:29:09 -0300

    arquivo pom

---
+++ src/main/java/br/com/eugenesistemas/projetomodelo/projetomodelo/model/Cliente.java
@@ -0,0 +1,16 @@
+package br.com.eugenesistemas.projetomodelo.projetomodelo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    private String nome;
+
+}
---
+++ src/test/java/br/com/eugenesistemas/projetomodelo/projetomodelo/model/ClienteTest.java
@@ -0,0 +1,25 @@
+package br.com.eugenesistemas.projetomodelo.projetomodelo.model;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import pl.pojo.tester.api.assertion.Method;
+import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
+
+@ExtendWith(MockitoExtension.class)
+class ClienteTest {
+
+    @Test
+    public void teste(){
+
+       final Class<?> cliente = Cliente.class;
+
+        assertPojoMethodsFor(cliente).testing(Method.GETTER, Method.SETTER).areWellImplemented();
+        assertPojoMethodsFor(cliente).testing(Method.CONSTRUCTOR).areWellImplemented();
+    }
+
+
+
+}
